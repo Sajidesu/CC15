40362cc0271a14b39d1814ebdd49cbe628513e29
NeedyRover9726 <89884077+NeedyRover9726@users.noreply.github.com>
2026-05-30 20:00:56 +0800

    Update gwapoko.sql

--- gwapoko.sql
+++ gwapoko.sql
@@ -18,6 +18,7 @@ CREATE TABLE admin_logs (
 	user_id INT NOT NULL,
 	action_taken VARCHAR(255) NOT NULL,
     action_date DATETIME DEFAULT CURRENT_TIMESTAMP,
+    phone_number VARCHAR(20),
     
     FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 
@@ -33,6 +34,9 @@ CREATE TABLE employee_logs (
 	log_date DATE NOT NULL,
     time_in DATETIME NOT NULL,
 	time_out DATETIME,
+    phone_number VARCHAR(20),
+    
+    
     
 	category ENUM('Part-Time', 'Full-Time') DEFAULT 'Full-Time',
 	status ENUM('on_time', 'late', 'absent')  DEFAULT 'on_time',
@@ -53,7 +57,8 @@ CREATE TABLE employee_info (
 	email VARCHAR(100) NOT NULL,
 	category ENUM('Part-Time', 'Full-Time') DEFAULT 'Full-Time',
 	department VARCHAR(100),
-    
+    birth_date DATE NOT NULL, 
+    phone_number VARCHAR(20)  NOT NULL,
 
 	FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE
 
@@ -75,18 +80,20 @@ VALUES
 (3, 'Joie Ann', 'Mac', 'BSEMC', '2026-05-28', '2026-05-28 08:01:28', 'on_time', 'Full-Time');
 
 
-INSERT INTO admin_logs (user_id, action_taken)
+INSERT INTO admin_logs (user_id, action_taken, phone_number)
 VALUES
-(4, 'Manually corrected Gefferson Balase attendance record');
+(4, 'Manually corrected Gefferson Balase attendance record', '09281734' );
+
+INSERT INTO employee_info (user_id, first_name, last_name, email, category, department, phone_number, birth_date)
+VALUES
+(1, 'Jordan', 'Cantete', 'jordan@test.com', 'Full-Time', 'BSCS','09281734', '1975-07-26'),
+(2, 'Gefferson', 'Balase', 'gefferson@test.com', 'Part-Time', 'BSCS','09281734',  '1980-07-26'),
+(3, 'Joie Ann', 'Mac', 'joie@test.com', 'Full-Time', 'BSEMC','09281734', '1980-10-14');
 
 SHOW TABLES;
 SELECT * FROM employee_logs;
 SELECT * FROM users;
 SELECT * FROM admin_logs;
+SELECT * FROM employee_info;
 
-INSERT INTO employee_info (user_id, first_name, last_name, email, category, department)
-VALUES
-(1, 'Jordan', 'Cantete', 'jordan@test.com', 'Full-Time', 'BSCS'),
-(2, 'Gefferson', 'Balase', 'gefferson@test.com', 'Part-Time', 'BSCS'),
-(3, 'Joie Ann', 'Mac', 'joie@test.com', 'Full-Time', 'BSEMC');
 
